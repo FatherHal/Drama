461911433e9d8620841507aae58ac371e6db7f0c
kek7198 <grayboy99@protonmail.com>
2021-11-23 17:13:47 -0600

    fix

--- files/assets/js/submission_listing.js
+++ files/assets/js/submission_listing.js
@@ -1,7 +1,7 @@
 // Expand Post
 
 function expandText(id) {
-	document.getElementById('post-text-'+id).classList.toggle('d-none');
+	document.getElementById('post-text-'+id).classList.toggle('hidden');
 	document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-expand-alt');
 	document.getElementsByClassName('text-expand-icon-'+id)[0].classList.toggle('fa-compress-alt');
 };
