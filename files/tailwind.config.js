db221672c205e867cd1d6d668b63b67f1e9c2a27
fireworks88 <throwaway.546372819@protonmail.com>
2021-11-24 23:20:44 +0100

    ffs

---
+++ files/tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+	//mode: 'jit',
+	purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+  	colors: {
+  		transparent: 'transparent',
+  		current: 'currentColor',
+  		black: colors.black,
+  		white: colors.white,
+  		gray: colors.gray,
+  		pink: colors.pink,
+  		green: colors.green,
+  		red: colors.rose,
+  		yellow: colors.amber
+  	},
+  	extend: {},
+  },
+  variants: {
+  	extend: {},
+  },
+  plugins: [],
+}
