cdaae60ce19021aa46b2c8432806ab4f44bf61e7
fireworks88 <throwaway.546372819@protonmail.com>
2021-11-25 19:45:23 +0100

    gzdezgdq

---
+++ files/postcss.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+console.log(__dirname);
+
+module.exports = (ctx) => ({
+  plugins: [
+    require('tailwindcss')(path.resolve(__dirname, 'tailwind.config.js')),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        path.resolve(__dirname, 'templates/**/*.html')
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  ],
+});
