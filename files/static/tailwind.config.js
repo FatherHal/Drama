926b956bae4e4f06cb3d17915b57def61d764729
fireworks88 <throwaway.546372819@protonmail.com>
2021-11-25 18:24:01 +0100

    test config

--- files/static/tailwind.config.js
+++ files/static/tailwind.config.js
@@ -3,6 +3,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
 	//mode: 'jit',
 	purge: [],
+    prefix: 'tw-',
   darkMode: 'class', // or 'media' or 'class'
   theme: {
   	colors: {
