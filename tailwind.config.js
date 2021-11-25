2ccdaa4b8327eb43052c5ab1460b9e29a4aa3cd9
fireworks88 <throwaway.546372819@protonmail.com>
2021-11-25 19:25:54 +0100

    sex

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+	//mode: 'jit',
+	purge: [],
+    prefix: 'x-',
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+  	colors: {
+  		transparent: 'transparent',
+  		current: 'currentColor',
+  		black: colors.black,
+  		white: colors.white,
+  		gray: colors.gray,
+  		pink: colors.pink,
+  		green: colors.green,
+  		red: colors.rose,
+  		yellow: colors.amber
+  	},
+  	extend: {},
+  },
+  variants: {
+  	extend: {},
+  },
+  plugins: [],
+}
